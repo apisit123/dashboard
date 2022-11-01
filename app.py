3dde4edf66a89905640fd080aa22c38bcecb37c0
apisit123 <aew.apisit@gmail.com>
2022-11-01 11:44:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import time  # to simulate a real time data, time loop
 
+#
+
 import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
 import plotly.express as px  # interactive charts
